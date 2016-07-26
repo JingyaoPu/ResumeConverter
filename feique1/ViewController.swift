e279367fe42709e13263e57a556e642a799492b0
DandlineNick <a1@1demacbook-pro.local>
2016-07-26 22:10:13 +0800

    Initial Commit

---
+++ feique1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  feique1
+//
+//  Created by 1 on 16/7/26.
+//  Copyright © 2016年 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ feique1Tests/feique1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  feique1Tests.swift
+//  feique1Tests
+//
+//  Created by 1 on 16/7/26.
+//  Copyright © 2016年 1. All rights reserved.
+//
+
+import XCTest
+@testable import feique1
+
+class feique1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
